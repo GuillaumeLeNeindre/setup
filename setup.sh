7bf0410bbbf8841ff4e5d108e5a20873a04c51e7
Ubuntu <ubuntu@ip-172-31-17-225.us-west-2.compute.internal>
2013-07-19 20:37:49 +0000

    Add nodejs

--- setup.sh
+++ setup.sh
@@ -46,3 +46,9 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+# install nodejs
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
